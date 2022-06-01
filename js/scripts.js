d4cf4b4fda9be4af143820941857cd759d6d900c
Claire Thorington <claire@Claires-iMac.local>
2022-06-01 16:28:54 -0700

    add list items and functionality

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+  $(".javascript-clickable").click(function(){
+      $("#javascript-showing").toggle();
+      $("#javascript-hidden").toggle();
+  })
+    $(".operators-clickable").click(function() {
+      $("#operators-showing").toggle();
+      $("#operators-hidden").toggle();
+  })
+    $(".variables-clickable").click(function() {
+      $("#variables-showing").toggle();
+      $("#variables-hidden").toggle();
+    })
+
+    $(".variable-conventions-clickable").click(function() {
+      $("#variable-conventions-showing").toggle();
+      $("#variable-conventions-hidden").toggle();
+  })
+
+  $(".functions-clickable").click(function() {
+    $("#functions-showing").toggle();
+    $("#functions-hidden").toggle();
+  })
+
+  $(".methods-clickable").click(function() {
+    $("#methods-showing").toggle();
+    $("#methods-hidden").toggle();
+  })
+
+  $(".arguments-clickable").click(function() {
+    $("#arguments-showing").toggle();
+    $("#arguments-hidden").toggle();
+  })
+
+  $(".parameters-clickable").click(function() {
+    $("#parameters-showing").toggle();
+    $("#parameters-hidden").toggle();
+  })
+
+  $(".return-clickable").click(function() {
+    $("#return-showing").toggle();
+    $("#return-hidden").toggle();
+  })
+
+  $(".chaining-methods-clickable").click(function() {
+    $("#chaining-methods-showing").toggle();
+    $("#chaining-methods-hidden").toggle();
+  })
+
+  $(".strings-clickable").click(function() {
+    $("#strings-showing").toggle();
+    $("#strings-hidden").toggle();
+  })
+
+  $(".booleans-clickable").click(function() {
+    $("#booleans-showing").toggle();
+    $("#booleans-hidden").toggle();
+  })
+
+  $(".undefined-clickable").click(function() {
+    $("#undefined-showing").toggle();
+    $("#undefined-hidden").toggle();
+  })
+
+  $(".nan-clickable").click(function() {
+    $("#nan-showing").toggle();
+    $("#nan-hidden").toggle();
+  })
+
+  $(".null-clickable").click(function() {
+    $("#null-showing").toggle();
+    $("#null-hidden").toggle();
+  })
+
+  $(".alert-clickable").click(function() {
+    $("#alert-showing").toggle();
+    $("#alert-hidden").toggle();
+  })
+
+  $(".comments-clickable").click(function() {
+    $("#comments-showing").toggle();
+    $("#comments-hidden").toggle();
+  })
+
+  $(".jquery-clickable").click(function() {
+    $("#jquery-showing").toggle();
+    $("#jquery-hidden").toggle();
+  })
+
+  $(".attributes-clickable").click(function() {
+    $("#attributes-showing").toggle();
+    $("#attributes-hidden").toggle();
+  })
+
+
+  });
